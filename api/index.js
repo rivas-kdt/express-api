bb3d4eeabd9a025601cd079a695e43ca9de29edb
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 07:38:56 +0800

    my api commit

--- api/index.js
+++ api/index.js
@@ -8,4 +8,11 @@ app.listen(3000, () => console.log("Server ready on port 3000."));
 
 app.get('/photos?id=:id', photos)
 
+app.get('/api/users/:id', (req, res) => {
+    res.json({
+      id: req.params.id,
+      message: `Fetching user with ID: ${req.params.id}`
+    });
+  });
+
 module.exports = app;
