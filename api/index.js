8ed7ee89eb8d8266b20acbd48254069ea8e8770e
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 07:42:23 +0800

    my api commit

--- api/index.js
+++ api/index.js
@@ -10,19 +10,16 @@ app.get("/photos/id=:id", async (req, res) => {
   try {
     const sql = neon(process.env.DATABASE_URL);
     const photoId = req.params.id;
-
     const [photo] = await sql`
               SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
                      a.title as album_title
               FROM photos p
               JOIN albums a ON p.album_id = a.id
-              WHERE p.id = ${photoId}
+              WHERE p.id = ${req.params.id}
             `;
-
     if (!photo) {
       res.status(404).send("Photo not found ");
     }
-
     res.status(200).send(photo);
   } catch (error) {
     res.status(500).send("Error Fetching Photo ");
