4b49c11886ff53eabb1f52db8ff6dd5ebc3e6a48
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 07:30:51 +0800

    my api commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express"
+import { photos } from "../handlers/photos"
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
+
+app.get('/photos?id=:id', photos)
 
 module.exports = app;
