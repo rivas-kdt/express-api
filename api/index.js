8375d501f731c55d842d70a05339f922c6af1f8a
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 07:58:13 +0800

    my api commit

--- api/index.js
+++ api/index.js
@@ -13,17 +13,21 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get("/users/id=:id", async (req, res) => {
+app.get("/photo/id=:id", async (req, res) => {
   try {
-    const users = await sql`
-        SELECT * from users WHERE id = ${req.params.id}
+    const photos = await sql`
+      SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
+             a.title as album_title
+      FROM photos p
+      JOIN albums a ON p.album_id = a.id
+      WHERE p.id = ${req.params.id}
       `;
 
-    if (!users) {
+    if (!photos) {
       return res.status(404).json({ error: "Photo not found" });
     }
 
-    res.status(200).json(users);
+    res.status(200).json(photos);
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Error fetching users" });
