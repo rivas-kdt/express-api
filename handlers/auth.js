7c7e95d71a50cb87b824bdabab9b9e8ecaa405f1
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 09:37:29 +0800

    my api commit

---
+++ handlers/auth.js
@@ -0,0 +1,31 @@
+import { neon } from "@neondatabase/serverless";
+import { bcrypt } from "bcryptjs";
+import { jwt } from "jsonwebtoken";
+import "dotenv/config.js";
+
+const sql = neon(process.env.DATABASE_URL);
+
+exports.login = async (req, res) => {
+  try {
+    const users = await sql`SELECT * FROM users WHERE email = ${email}`;
+    if (users.length === 0) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const user = users[0];
+    const passwordMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!passwordMatch) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const token = jwt.sign({id: users.id}, process.env.JWT_SECRET);
+    res.status(200).json(token)
+  } catch (error) {}
+};
