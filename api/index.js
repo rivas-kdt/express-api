6a553000f3b45c3b427258210da4e1e8e64d9c51
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 08:01:59 +0800

    my api commit

--- api/index.js
+++ api/index.js
@@ -13,26 +13,43 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get("/photo/id=:id", async (req, res) => {
+app.get("/users/id=:id", async (req, res) => {
   try {
-    const photos = await sql`
-      SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
-             a.title as album_title
-      FROM photos p
-      JOIN albums a ON p.album_id = a.id
-      WHERE p.id = ${req.params.id}
+    const users = await sql`
+        SELECT * from users WHERE id = ${req.params.id}
       `;
 
-    if (!photos) {
+    if (!users) {
       return res.status(404).json({ error: "Photo not found" });
     }
 
-    res.status(200).json(photos);
+    res.status(200).json(users);
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Error fetching users" });
   }
 });
+
+app.get("/photo", async (req, res) => {
+    try {
+      const photos = await sql`
+        SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
+               a.title as album_title
+        FROM photos p
+        JOIN albums a ON p.album_id = a.id
+        WHERE p.id
+        `;
+  
+      if (!photos) {
+        return res.status(404).json({ error: "Photo not found" });
+      }
+  
+      res.status(200).json(photos);
+    } catch (error) {
+      console.error("Error fetching photo:", error);
+      res.status(500).json({ error: "Error fetching photo" });
+    }
+  });
 
 app.get("/api/users/:id", (req, res) => {
   res.json({
