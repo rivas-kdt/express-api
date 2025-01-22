ea5877026cbcddca0f61ede28bead1d950bd6202
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 10:05:50 +0800

    my api commit

--- handlers/auth.js
+++ handlers/auth.js
@@ -25,6 +25,9 @@ exports.login = async (req, res) => {
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
+    if(!token){
+        res.status(404).json("Can't Generate Token")
+    }
     res.status(200).json(token);
   } catch (error) {
     res.status(500).json("Error");
