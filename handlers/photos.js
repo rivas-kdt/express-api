87661c076f813a3e5f50de0c346ad4beafe023e9
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 08:12:28 +0800

    my api commit

--- handlers/photos.js
+++ handlers/photos.js
@@ -5,12 +5,7 @@ const sql = neon(process.env.DATABASE_URL);
 
 exports.photos = async (req, res) => {
   try {
-    const photos = await sql`
-      SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
-             a.title as album_title
-      FROM photos p
-      JOIN albums a ON p.album_id = a.id
-      `;
+    const photos = await sql`SELECT * FROM photos`;
 
     if (!photos) {
       return res.status(404).json({ error: "Photo not found" });
