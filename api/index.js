1a4ce7c5b084a1c89b8fc90779e64d53ab6a4f38
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-23 06:42:27 +0800

    fixes cors policy allowed origin

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 const corsOptions = {
-  origin: "https://neon-photo-ad.vercel.app/", // Frontend URL
+  origin: "https://neon-photo-ad.vercel.app", // Frontend URL
   credentials: true, // Allow cookies to be sent
 };
 
