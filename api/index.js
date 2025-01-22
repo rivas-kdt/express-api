ac1a584da8ec235c161b3fd61370468d9c6593d3
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 08:10:42 +0800

    my api commit

--- api/index.js
+++ api/index.js
@@ -1,54 +1,17 @@
 import express from "express";
 import cors from "cors";
-import { neon } from "@neondatabase/serverless";
-import "dotenv/config.js";
+import { photos } from "../handlers/photos"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const sql = neon(process.env.DATABASE_URL);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get("/users/id=:id", async (req, res) => {
-  try {
-    const users = await sql`
-        SELECT * from users WHERE id = ${req.params.id}
-      `;
-
-    if (!users) {
-      return res.status(404).json({ error: "Photo not found" });
-    }
-
-    res.status(200).json(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Error fetching users" });
-  }
-});
-
-app.get("/photo", async (req, res) => {
-    try {
-      const photos = await sql`
-        SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
-               a.title as album_title
-        FROM photos p
-        JOIN albums a ON p.album_id = a.id
-        `;
-  
-      if (!photos) {
-        return res.status(404).json({ error: "Photo not found" });
-      }
-  
-      res.status(200).json(photos);
-    } catch (error) {
-      console.error("Error fetching photo:", error);
-      res.status(500).json({ error: "Error fetching photo" });
-    }
-  });
+app.get("/photos", photos);
 
 app.get("/api/users/:id", (req, res) => {
   res.json({
--- handlers/photos.js
+++ handlers/photos.js
@@ -1,24 +1,24 @@
 import { neon } from "@neondatabase/serverless";
+import "dotenv/config.js";
+
+const sql = neon(process.env.DATABASE_URL);
 
 exports.photos = async (req, res) => {
   try {
-    const sql = neon(process.env.DATABASE_URL);
-    const photoId = req.params.id;
+    const photos = await sql`
+      SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
+             a.title as album_title
+      FROM photos p
+      JOIN albums a ON p.album_id = a.id
+      `;
 
-    const [photo] = await sql`
-          SELECT p.id, p.title, p.description, p.file_url, p.created_at, p.album_id,
-                 a.title as album_title
-          FROM photos p
-          JOIN albums a ON p.album_id = a.id
-          WHERE p.id = ${photoId}
-        `;
-
-    if (!photo) {
-      res.status(404).send("Photo not found ");
+    if (!photos) {
+      return res.status(404).json({ error: "Photo not found" });
     }
 
-    res.status(200).send(photo);
+    res.status(200).json(photos);
   } catch (error) {
-    res.status(500).send("Error Fetching Photo ");
+    console.error("Error fetching photo:", error);
+    res.status(500).json({ error: "Error fetching photo" });
   }
 };
