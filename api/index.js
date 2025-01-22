d65df88af681d16b84c92b9d12c8a428c6fc5972
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-22 13:47:13 +0800

    imported multer on albums.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000']
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
