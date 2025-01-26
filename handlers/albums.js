39f6bb1c357a5a30cad40fd2d0fde31068b053fb
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-27 07:48:31 +0800

    api fix albums/user

--- handlers/albums.js
+++ handlers/albums.js
@@ -44,8 +44,8 @@ export const albumByUser = async (req, res) => {
       res.status(401).json("Unauthenticated");
     }
     const id = claims.id;
-    let albums = [];
-    albums = await sql`SELECT * FROM albums WHERE user_id = ${id}`;
+    const response = await sql`SELECT * FROM albums WHERE user_id = ${id}`;
+    const albums = response ? response : [];
     res.status(200).json(albums);
   } catch (error) {
     console.error("Error Fetching User's Albums");
